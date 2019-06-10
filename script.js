9664bb4227c45adb663f06c5d27efb76734844fe
Suzanne Smigielski <ssmigiel571@gmail.com>
2019-06-10 18:46:13 -0500

    api functions

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 $(document).ready(function(){
-    $(".submit").click(){
-        $('.num-dogs').val();
-    }
+    
+    function getDogPics(){
+        fetch('https://dog.ceo/api/breeds/image/random/3');
+        .then(response => response.json())
+        .then(responseJson =>
+            displayResults(responseJson))
+        .catch(error => alert("Something went wrong, try again."));
 
+   }
+
+
+function displayResults(responseJson){
+        console.log(responseJson);
+        $('.results-img').replaceWith(<img src="${responseJson.message}" class="results-img">
+        </img>)
+        $('.results').show();
+   }
+
+function runForm(){
+    $('form').submit(e => {
+        e.preventDefault();
+        getDogPics();
+    });
+}
+
+runForm();
 
 
 
