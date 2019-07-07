b064638c129c45ee42b96041ddddc2644b20336d
Suzanne Smigielski <ssmigiel571@gmail.com>
2019-07-07 16:29:24 -0500

    removed assignment1 code

--- script.js
+++ script.js
@@ -1,43 +1,5 @@
 $(document).ready(function(){
 
-
-
-    function getRandomDogPics(){
-        let api = 'https://dog.ceo/api/breeds/image/random';
-        let userNum = $('#num-dogs').val();
-        //let userNum = document.getElementById('num-dogs');        
-        let apiPath = api + '/'+ userNum;
-        console.log(userNum);
-        //console.log(userNum.value);
-        console.log(apiPath);
-
-        fetch(apiPath)
-        .then(response => response.json())
-        .then(responseJson =>
-            displayNumResults(responseJson))
-        .catch(error => alert("Something went wrong, try again."));
-
-   }
-
-
-function displayNumResults(responseJson){
-        console.log(responseJson);
-        
-       //responseJson === {status:" ", message: [""]}
-       //output is 3 <img> displayed .results-img
-
-       //for each image URL string within message create <img> html string using URL as src
-       let imgHtmlStrings = responseJson.message.map(imgUrlString => { 
-       return `<img src="${imgUrlString}" class="results-img"></img>`
-       });
-
-       //combine seperate <img> strings into one. 
-       let combinedString = imgHtmlStrings.join('');
-       //display combined <img> string created in results-img 
-       $('.imgContainer').html(combinedString);
-       $('.results').removeClass('hidden');
-   }
-
 function getDogBreed(){
     let api2 = 'https://dog.ceo/api/breed';
     let userText = $('#breeds').val();
@@ -67,15 +29,6 @@ function displayBreedResults(responseJson){
 }
 
 
-
-function runForm(){
-    $('form').submit(e => {
-        e.preventDefault();
-        getRandomDogPics();
-        
-    });
-}
-
 function runForm2(){
     $('form').submit(e=> {
         e.preventDefault();
@@ -84,7 +37,7 @@ function runForm2(){
 
 }
 
-runForm();
+
 runForm2();
 
 
